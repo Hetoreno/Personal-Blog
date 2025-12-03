0d2e8fbb042f64e7f49f2b737639b4aa045a5d64
Hetoreno <hectortamriel@gmail.com>
2025-12-02 22:51:12 -0800

    adding custom fields

--- app/public/wp-content/themes/blankslate/page-home.php
+++ app/public/wp-content/themes/blankslate/page-home.php
@@ -3,20 +3,24 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<?php the_content(); ?>
-<section id="intro">
+<!-- Intro to Home Page -->
 
-        <div class="intro-container">
-            <div class="overlay"></div>
-            <div class="intro-text">
-                <h2>Welcome to my Blog!</h2>
-                <p>This is where I will update posts of my current everyday activities like my hobbies or my passion to code. </p>
-            </div>
+    <section id="intro">
+    <div class="intro-container" style="background-image: url(<?php the_field('hero_image'); ?>);">
+        <div class="overlay"></div>
+        <div class="intro-text">
+            <h2><?php echo esc_html( get_field('hero_title') ); ?></h2>
+            <p>This is where I will update posts of my current everyday activities like my hobbies or my passion to code. </p>
         </div>
-        
-        <!-- to-top arrow -->
-        <a href="#" class="to-top"><i class="fas fa-sort-up"></i></a>
-    </section>
+    </div>
+    <!-- to-top arrow -->
+    <a href="#" class="to-top"><i class="fas fa-sort-up"></i></a>
+</section>
+
+
+<!-- Article Section -->
+<?php the_content(); ?>
+
 
 <?php endwhile; endif; ?>
 
